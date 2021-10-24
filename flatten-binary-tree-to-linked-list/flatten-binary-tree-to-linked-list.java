f79d8ad998d2f35ae0238e441973262195574f7e
jessi68 <47855517+jessi68@users.noreply.github.com>
2021-10-24 22:18:51 +0900

    Update flatten-binary-tree-to-linked-list.java

--- flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -16,21 +16,57 @@
 
 public class Solution {
     public void flatten(TreeNode root) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        if(root==null) return ; 
+       
+        if(root==null) return; 
         TreeNode left=root.left; 
         TreeNode right=root.right; 
+        
         if(left!=null)
         {
             root.right=left; 
             root.left=null; 
-            TreeNode rightMost=root.right; 
-            while(rightMost!=null && rightMost.right!=null)
+            TreeNode mostRightNode=root.right; 
+            while(mostRightNode!=null && mostRightNode.right!=null)
             {
-                rightMost=rightMost.right; 
+                mostRightNode=mostRightNode.right; 
             }
-            if(rightMost!=null) rightMost.right=right; 
+            if(mostRightNode!=null) mostRightNode.right=right; 
+        }
+        flatten(root.right);
+    }
+}/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public void flatten(TreeNode root) {
+       
+        if(root==null) return; 
+        TreeNode left=root.left; 
+        TreeNode right=root.right; 
+        
+        if(left!=null)
+        {
+            root.right=left; 
+            root.left=null; 
+            TreeNode mostRightNode=root.right; 
+            while(mostRightNode!=null && mostRightNode.right!=null)
+            {
+                mostRightNode=mostRightNode.right; 
+            }
+            if(mostRightNode!=null) mostRightNode.right=right; 
         }
         flatten(root.right);
     }
